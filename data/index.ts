54b2c81b21fb6cc1cd767a369b76eb0861aea97d
Oloyede Israel <etzisrael912.ope@gmail.com>
2025-05-02 04:10:36 +0100

    push repository

---
+++ data/index.ts
@@ -0,0 +1,236 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "An AI Chatbox Using React Frameworks",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/brainwave.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://enchanting-concha.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/iphone.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://charming-cannoli.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "A 3D Gaming Website Using Next.js",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/experience.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://roaring-biscuit.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "A Sneakers Ordering Website Landing Page",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/comlab stores.jpg",
+    iconLists: ["/html5.svg", "/css3.svg", "/javascript.svg"],
+    link: "https://comlabstores.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Portfolio Design Using HTML CSS and Javascript Framework",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/anointed1.jpg",
+    iconLists: ["/html5.svg", "/css3.svg", "/javascript.svg", "/gsap.svg"],
+    link: "https://anointed1.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "E-commerce Landing Page for Foodstuffs Orders",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/e-commerce.jpg",
+    iconLists: ["/html5.svg", "/css3.svg", "/javascript.svg"],
+    link: "https://delicacy-home.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Anointed1 has been an absolute pleasure. His expertise in frontend development is unparalleled. He transformed our website's user interface with sleek, responsive designs that have significantly enhanced user experience. The attention to detail and ability to meet deadlines have made him an invaluable part of our team. I would highly recommend Anointed1 for any frontend development needs.",
+    name: "Laura D.",
+    title: "Senior Product Manager at InnovateTech",
+  },
+  {
+    quote:
+      "As a startup, we needed a frontend developer who could not only bring our vision to life but also create a design that was modern and user-friendly. Anointed1 exceeded all expectations. He provided creative solutions that aligned perfectly with our brand and helped streamline our website. His communication throughout the process was excellent, making collaboration seamless. Truly a top-tier professional.",
+    name: "Carlos P.",
+    title: "Co-Founder of GreenVision",
+  },
+  {
+    quote:
+      "Anointed1 is a frontend development guru. We hired him to revamp our e-commerce platform, and the results were spectacular. The new interface is not only visually appealing but also optimized for performance across all devices. Anointed1 worked diligently to ensure the project was delivered on time and handled every request with expertise. If you want a polished and effective frontend, Anointed1 is the developer you need.",
+    name: "Samantha L.",
+    title: "E-Commerce Director at Comlabstores",
+  },
+  {
+    quote:
+      "I’ve had the pleasure of collaborating with Anointed1 on multiple projects, and each time, his skills have impressed me. He consistently bring fresh, innovative ideas to the table, combined with a strong understanding of frontend technologies. What sets Anointed1 apart is his ability to take complex requirements and transform them into intuitive, beautiful interfaces. Highly recommended for any project requiring cutting-edge frontend development.",
+    name: "Tom R.",
+    title: "CTO at TechNova Solutions",
+  },
+  {
+    quote:
+      "We were looking for a frontend developer who could help us refine our website’s design and functionality, and Anointed1 delivered beyond our expectations. His responsiveness, commitment to quality, and thorough understanding of the latest web trends made a real difference. The new design is clean, easy to navigate, and truly reflects our brand. I look forward to working with Anointed1 on future projects.",
+    name: "Rachel M.",
+    title: "Marketing Director at PureFlare",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Devoloper Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-friendly features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/israelope",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/Olarmihlehkan",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "/linkedin.com",
+  },
+];
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://95f34fa0d294499002ebeb7f2cefb281@o4509250388099072.ingest.de.sentry.io/4509251223814224",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
